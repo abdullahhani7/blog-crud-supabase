5303345f2be57805a7026e95067619362b13fbe6
Abdullah <abdulhhany69@gmail.com>
2026-01-06 10:01:10 +0200

    `Added state management and form handling to CreateBlog component`

--- src/pages/CreateBlog.jsx
+++ src/pages/CreateBlog.jsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
+
 export default function CreateBlog() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = () => {
+    console.log(title, description);
+  };
+
   return (
     <div className="form-container">
       <h2>Create a new Blog</h2>
 
-      <input placeholder="Write your title..." />
-      <textarea placeholder="Write your description..." />
-
-      <button className="save-btn">Save Blog</button>
+      <input
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="Write your title..."
+      />
+      <textarea
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        placeholder="Write your description..."
+      />
+      <p style={{color:'red', fontSize:"12px"}}>* please fill all the field</p>
+      <button  onClick={handleSubmit} className="save-btn">
+        Save Blog
+      </button>
     </div>
   );
 }
