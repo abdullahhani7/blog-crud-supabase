374ee71f484e90fbb887c206f26c8f42b8ee6ba9
Abdullah <abdulhhany69@gmail.com>
2026-01-06 09:42:27 +0200

    feat: integrate Supabase for blog data management and add environment configuration

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,32 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Blogs from "./pages/Blogs";
 import CreateBlog from "./pages/CreateBlog";
 import EditBlog from "./pages/EditBlog";
+import supabase from "./supabase";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  // console.log(supabase);
+
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const getBlogs = async () => {
+      const { data, error } = await supabase.from("blogs").select();
+      // console.log(data);
+      if (error) {
+        return alert(error);
+      }
+      if (data) {
+        setBlogs(data);
+      }
+    };
+    getBlogs();
+  }, []);
+
   return (
     <BrowserRouter>
       <header className="header">
-        <Link to="/create" className="header-logo">
+        <Link to="/" className="header-logo">
           <h2>Blog</h2>
         </Link>
 
@@ -17,7 +37,7 @@ export default function App() {
       </header>
 
       <Routes>
-        <Route path="/" element={<Blogs />} />
+        <Route path="/" element={<Blogs blogs={blogs} setBlogs={setBlogs}/>} />
         <Route path="/create" element={<CreateBlog />} />
         <Route path="/edit/:id" element={<EditBlog />} />
       </Routes>
--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,12 +1,11 @@
-import { useState } from "react";
 import BlogCard from "../components/BlogCard";
 
-export default function Blogs() {
-  const [blogs, setBlogs] = useState([
-    { id: 1, title: "Blog 1", description: "Lorem ipsum dolor sit amet" },
-    { id: 2, title: "Blog 2", description: "Lorem ipsum dolor sit amet" },
-    { id: 3, title: "Blog 3", description: "Lorem ipsum dolor sit amet" },
-  ]);
+export default function Blogs({ blogs, setBlogs }) {
+  // const [blogs, setBlogs] = useState([
+  //   { id: 1, title: "Blog 1", description: "Lorem ipsum dolor sit amet" },
+  //   { id: 2, title: "Blog 2", description: "Lorem ipsum dolor sit amet" },
+  //   { id: 3, title: "Blog 3", description: "Lorem ipsum dolor sit amet" },
+  // ]);
 
   const deleteBlog = (id) => {
     setBlogs(blogs.filter((b) => b.id !== id));
@@ -14,9 +13,10 @@ export default function Blogs() {
 
   return (
     <div className="blogs-container">
-      {blogs.map((blog) => (
-        <BlogCard key={blog.id} blog={blog} onDelete={deleteBlog} />
-      ))}
+      {blogs &&
+        blogs.map((blog) => (
+          <BlogCard key={blog.id} blog={blog} onDelete={deleteBlog} />
+        ))}
     </div>
   );
 }
