9f5ce76d6f85f24b7cd261a9562f90906d209faa
Abdullah <abdulhhany69@gmail.com>
2026-01-06 10:08:58 +0200

    `Added error handling to CreateBlog component`

--- src/pages/CreateBlog.jsx
+++ src/pages/CreateBlog.jsx
@@ -3,9 +3,16 @@ import { useState } from "react";
 export default function CreateBlog() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const [error, setError] = useState(false);
 
   const handleSubmit = () => {
-    console.log(title, description);
+    // console.log(title, description);
+    if (title === "" || description === "") {
+      setError(true);
+      return;
+    }
+
+    setError(false);
   };
 
   return (
@@ -14,16 +21,27 @@ export default function CreateBlog() {
 
       <input
         value={title}
-        onChange={(e) => setTitle(e.target.value)}
+        onChange={(e) => {
+          setTitle(e.target.value);
+          setError(false);
+        }}
         placeholder="Write your title..."
       />
       <textarea
         value={description}
-        onChange={(e) => setDescription(e.target.value)}
+        onChange={(e) => {
+          setDescription(e.target.value);
+          setError(false);
+        }}
         placeholder="Write your description..."
       />
-      <p style={{color:'red', fontSize:"12px"}}>* please fill all the field</p>
-      <button  onClick={handleSubmit} className="save-btn">
+      {error && (
+        <p style={{ color: "red", fontSize: "12px" }}>
+          * please fill all the field
+        </p>
+      )}
+
+      <button onClick={handleSubmit} className="save-btn">
         Save Blog
       </button>
     </div>
